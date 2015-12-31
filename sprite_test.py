f58c7aceb98d2ad9c2d2865a716067bc5fd7ab7f
Colin Bowers <colin.bowers@gmail.com>
2015-12-31 10:20:55 -0500

    Created Asteroid class. Manage list of sprites in main().

--- sprite_test.py
+++ sprite_test.py
@@ -2,7 +2,7 @@ import pygame, sys, math, random
 from pygame.locals import *
 
 FPS = 30
-MAX_X = 600
+MAX_X = 800
 MAX_Y = 600
 
 WHITE     = (255, 255, 255)
@@ -19,6 +19,91 @@ DISPLAYSURF = pygame.display.set_mode((MAX_X, MAX_Y))
 BASICFONT = pygame.font.SysFont('courier', 15)
 pygame.display.set_caption('pyGame Template')
 random.seed()
+
+
+class Asteroid():
+
+    ast_spritesheet = None
+    ast_img = []
+    exp_spritesheet = None
+    exp_img = []
+    pos = (0, 0)
+    vel = (0, 0)
+    state = 1
+    NORMAL = 1
+    EXPLODING = 2
+    DONE = 3
+    frame = 0
+
+    def __init__(self):
+        self.pos = (random.randint(48, MAX_X-48), random.randint(48, MAX_Y-48))
+        s = random.randint(3, 10)
+        self.vel = (random.randint(-s, s), random.randint(-s, s))
+        self.state = self.NORMAL
+
+        #TODO: have only 1 instance of the spritesheet.  use a class variable?
+
+        #load asteroid sprite
+        self.ast_spritesheet = pygame.image.load('assets/asteroid2.png')
+        self.ast_img = []
+        s = 64
+        for y in range(0,6):
+            for x in range (0, 5):
+                self.ast_img.append(self.ast_spritesheet.subsurface( (x*s,y*s,s,s) ))
+
+        #load explosion sprite
+        self.exp_spritesheet = pygame.image.load('assets/explosion1_64x64.png')
+        self.exp_img = []
+        s = 64
+        for y in range(0, 5):
+            for x in range (0, 5):
+                self.exp_img.append(self.exp_spritesheet.subsurface( (x*s,y*s,s,s) ))
+
+    def is_normal(self):
+        return self.state == self.NORMAL
+
+    def is_done(self):
+        return self.state == self.DONE
+
+    def is_exploding(self):
+        return self.state == self.EXPLODING
+
+    def start_exploding(self):
+        if self.state == self.NORMAL:
+            self.state = self.EXPLODING
+            self.vel = (0, 0)
+            self.frame = 0
+
+    def update(self):
+        x = self.pos[0] + self.vel[0]
+        y = self.pos[1] + self.vel[1]
+        if x >= MAX_X:  x = 0
+        if x < 0:       x = MAX_X
+        if y >= MAX_Y:  y = 0
+        if y < 0:       y = MAX_Y
+        self.pos = (x, y)
+
+    def draw(self):
+        x = self.pos[0] - 32
+        y = self.pos[1] - 32
+
+        if self.state == self.NORMAL:
+            img = self.ast_img[self.frame]
+            DISPLAYSURF.blit(img, (x, y))
+            self.frame += 1
+            if self.frame >= len(self.exp_img):
+                self.frame = 0
+
+        elif self.state == self.EXPLODING:
+            img = self.exp_img[self.frame]
+            DISPLAYSURF.blit(img, (x, y))
+            self.frame += 1
+            if self.frame >= len(self.exp_img):
+                self.frame = 0
+                self.state = self.DONE
+
+        else:
+            pass
 
 
 def terminate():
@@ -43,28 +128,40 @@ def handle_events():
 
 def main():
 
-    img = pygame.image.load('assets/explosion1_64x64.png')
     sprites = []
-    s = 64
-    for y in range(0, 5):
-        for x in range (0, 5):
-            sprites.append(img.subsurface( (x*s,y*s,s,s) ))
+    sprites.append( Asteroid() )
 
-    i = 0
-    sprite_done = False
-    pos = (random.randint(48, MAX_X-48), random.randint(48, MAX_Y-48))
+    #main game loop
     while True:
-        handle_events()
+
+       #event handling
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+
+            elif event.type == KEYUP:
+                if event.key == K_SPACE:
+                    for i in range(0, len(sprites)):
+                        if sprites[i].is_normal():
+                            sprites[i].start_exploding()
+                            break;
+
+                if event.key == K_n:
+                    sprites.append( Asteroid() )
+                if event.key == K_ESCAPE:
+                    terminate()
+
+
+        #update game objects
+        if len(sprites) > 0 and sprites[0].is_done():
+            sprites = sprites[1:]
+        for s in sprites:
+            s.update()
 
         # draw frame
         DISPLAYSURF.fill(BGCOLOR)
-
-        DISPLAYSURF.blit(sprites[i], pos)
-
-        i += 1
-        if i >= len(sprites):
-            i = 0
-            pos = (random.randint(48, MAX_X-48), random.randint(48, MAX_Y-48))
+        for s in sprites:
+            s.draw()
 
         #pygame.display.update()
         pygame.display.flip()
