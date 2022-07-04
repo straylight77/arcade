91d287dbfbcae15a7e6fc70eaac09199fc4790fc
Colin Bowers <colin.bowers@gmail.com>
2022-07-03 22:26:55 -0400

    Refactor now has very basic moving between rooms.

--- text-adventures/main2.py
+++ text-adventures/main2.py
@@ -5,12 +5,11 @@ import json, textwrap, cmd
 SCREEN_WIDTH = 80
 
 rooms = { }
+player = None
 
 
 # =========================================================================
 class Base():
-
-    name = ""
 
     # ---------------------------------------------------------------------
     def __init__(self, name):
@@ -27,14 +26,21 @@ class Base():
 
 # =========================================================================
 class Room(Base):
-    exits = { }
-    items = { }
-    props = { }
 
     # ---------------------------------------------------------------------
     def __init__(self, name, desc):
         super().__init__(name)
         self.desc = desc
+        self.exits = { }
+
+    # ---------------------------------------------------------------------
+    def description(self):
+        room_str = "\n" + self.name.upper() + "\n"
+        room_str += self.desc
+        for d, r in self.exits.items():
+            room_str += f"\n{d.capitalize()}: {r}"
+
+        return room_str
 
 
 # =========================================================================
@@ -55,6 +61,15 @@ class Container(Prop, set):
     searchable = False
     openable = False
 
+    def do_search(self):
+        pass
+
+    def do_open(self):
+        pass
+
+    def do_unlock(self):
+        pass
+
 
 # =========================================================================
 class Barrier(Prop):
@@ -73,19 +88,57 @@ class Player():
 
 
 
-# **** main() *************************************************************
+# =========================================================================
+class MainGameCmd(cmd.Cmd):
+    prompt = '\n> '
+
+    # -------------------------------------------------------
+    def default(self, arg):
+        print('I do not understand that command. Type "help" for a list of commands.')
+
+    # -------------------------------------------------------
+    def do_quit(self, arg):
+        """Quit the game."""
+        return True
+
+    # -------------------------------------------------------
+    def do_look(self, arg):
+        """Look around your current location and get a description of what you see, visible exits, etc."""
+        global player
+        print(player.location.description())
+
+
+    # -------------------------------------------------------
+    def do_go(self, arg):
+        global player
+        if not arg in ['north', 'east', 'south', 'west']:
+            print("That's not a valid direction.")
+        elif not arg in player.location.exits.keys():
+            print("Moving in that direction isn't possible right now.")
+        else:
+            dest = player.location.exits[arg]
+            player.location = dest
+            print(f"You move {arg}.")
+            self.do_look(None)
+
+
+# **** init() *************************************************************
+def init():
+    global player, room
+    player = Player()
+    r1 = Room("White Room", "This is a long description of the test room.")
+    r2 = Room("Blue Room", "This is a long description of the test room.")
+    r1.exits['east'] = r2
+    r2.exits['west'] = r1
+    rooms["White Room"] = r1
+    rooms["Blue Room"] = r2
+    player.location = r1
+
+
+
+# **** main ***************************************************************
 if __name__ == '__main__':
-    name = "Test Room"
-    r = Room(name, "This is a long description of the test room.")
-    rooms[name] = r
-    print(rooms)
-
-    i = Item("hydrospanner")
-    print(i)
-
-    print( {"one":i, "two":i} )
-
-    p = Player()
-    p.inventory.add(i)
-    print(p.inventory)
+    init()
+    MainGameCmd().do_look(None)
+    MainGameCmd().cmdloop()
 
